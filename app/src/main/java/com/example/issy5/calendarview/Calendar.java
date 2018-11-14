8aa2e7098cf8eae2d0a7059c48e76b840eb0460f
IsmaeelatBrunel <1708632@brunel.ac.uk>
2018-11-14 12:20:39 +0000

    Initial commit

---
+++ app/src/main/java/com/example/issy5/calendarview/Calendar.java
@@ -0,0 +1,30 @@
+package com.example.issy5.calendarview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class Calendar extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        calendarView = (CalendarView) findViewById(R.id.calenderView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = i2 + "/" + (i1+1) + "/" + i;
+                myDate.setText(date);
+            }
+        });
+    }
+}
